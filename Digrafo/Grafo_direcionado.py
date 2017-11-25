c093f7a60ca38f08750884f5aa432db832ef33b4
daniel <daniel.rodrigues.45@hotmail.com>
2017-11-25 08:46:41 -0200

    Testes com o digrafo. BFS implementada.

--- Digrafo/Grafo_direcionado.py
+++ Digrafo/Grafo_direcionado.py
@@ -1,48 +1,83 @@
 # -*- coding: cp1252 -*-
 from aresta import Arestas
+import math
+import fila
 
-Type = True # True não instancia Arestas, false sim
-# Entre true e false, p/ 1 000 000, true é mais rapido para adicionar arestas
-
+# Vertice == {rotulo : arestas(rotulo, id)}
 class Grafo:
     def __init__(self):
         self.Vertices = {}
         self.num_vertices = 0
         self.id = 1
 
-    def add_vertice(self, rotulo):
-        if rotulo not in self.Vertices:
-            #print("{} - Vertice ainda nao existe".format(rotulo))
-            if (Type):
-                self.Vertices[rotulo] = [self.id, {}]
-            else:
-                self.Vertices[rotulo] = Arestas(rotulo, self.id)
+    def addVertice(self, rotulo, info=None):
+        if rotulo not in self.Vertices.keys():
+            self.Vertices[rotulo] = [info, Arestas(rotulo=rotulo, id=self.id)]
             self.id += 1
-        else:
-            #print("Vertice ja existe")
-            pass
-
+    
+    def getInfo(self, de):
+        return self.Vertices[de][0]
+    
+    def setInfo(self, de, info=None):
+        self.Vertices[de][0] = info
+    
     # info pode ser uma lista de itens( para fazer buscas, entre outras coisas )
-    def add_aresta(self, de, para, info=1):
+    def addAresta(self, de, para, info = {'peso': 1}):
         if de not in self.Vertices.keys():
-            self.add_vertice(de)
+            self.addVertice(de)
         if para not in self.Vertices.keys():
-            self.add_vertice(para)
+            self.addVertice(para)
 
-        if (Type):
-            self.Vertices[de][1][para] = info
-            ### sem direcao, entao
-            #self.Vertices[para][1][de] = info
-        else:
-            self.Vertices[de].add_vizinho( para, info )
-            ### sem direcao, entao
-            #self.Vertices[para].add_vizinho( de, info )
-            
+        self.Vertices[de][1].addAresta( para, info )
+
+    
+    def BFS(self, de, para=None):
+        #self.Vertices[de].setInfoV({'cor' : 'c', 'Du' : 0, 'pred' : None})
+        rel = []
+        rel.append(0)
+        
+        for v in self.Vertices:
+            self.setInfo(v, ['b',math.inf, None])
+        self.setInfo(de, ['c', 0, None])
+        
+        Q = fila.fila()
+        Q.push(de)
+        while not Q.vazia():
+            u = Q.pop()
+            infou = self.getInfo(u)
+            rel[infou[1]] += 1
+            naoB = 0
+                
+            for v in self.Vertices[u][1].getArestas():
+                infov = self.getInfo(v)
+                if infov[0] == 'b':
+                    self.setInfo(v, ['c', infou[1]+1, u])
+                    Q.push(v)
+                else:
+                    naoB += 1
+
+            if self.Vertices[u][1].getQtdeArestas() != naoB:
+                rel.append(0)
+            self.setInfo(u, ['p', infou[1], infou[2]])
+
+        print("Concluido")
+        if len(rel) > 1000:
+            atua = []
+            atua.append(0)
+            k = 0
+            for i in range(len(rel)):
+                atua[k] += rel[i]
+                if i % 100 == 0:
+                    atua.append(0)
+                    k += 1
+                    
+        print(atua[0:50])
+        print(rel[:50])
+    
+        
+    '''       
     def get_arestas_do_vertice(self, rotulo):
-        if(Type):
-            return self.Vertices[rotulo] if rotulo in self.Vertices else None
-        else:
-            return self.Vertices[rotulo].getInfo() if rotulo in self.Vertices else None
+        return self.Vertices[rotulo].getInfo() if rotulo in self.Vertices else None
 
     
     def get_grau_vertice(self, rotulo):
@@ -55,39 +90,38 @@ class Grafo:
                     if rotulo == aresta:
                         cont += 1
         return cont
-    
-    def printGrafo(self):
-        for i in self.Vertices.keys():
-            if(type):
-                print("{} - {}".format(i,self.Vertices[i]))
-            else:
-                tmp = self.Vertices[i].getInfo()
-                print("{} {} - {}".format(i,tmp[0], tmp[1]))
-                if len(tmp[2]) > 0:
-                    print(tmp[2])
+    '''
+    def print(self):
+        for key in self.Vertices.keys():
+            print("{} - {}".format(key, self.getInfo(key)))
+            #print(self.Vertices[key][1].getArestasInfo())
             
     
 
-'''
+
 g = Grafo()
 for i in range(1000000):
-    #print(i)
-    g.add_vertice(i)
+    g.addAresta(i, i+1)
 
-g.add_aresta(900000, 6, 10)
 print("Concluido")
+g.BFS(1)
+
+
 '''
-
-
 g = Grafo()
 
-g.add_aresta('A', 'B')
-g.add_aresta('B', 'C')
-g.add_aresta('C', 'D')
-g.add_aresta('D', 'A')
-
-g.add_vertice("A")
-g.add_vertice("E")
+g.addAresta('A', 'B')
+g.addAresta('A', 'C')
+g.addAresta('B', 'C')
+g.addAresta('C', 'D')
+g.addAresta('D', 'A')
+#g.print()
+g.BFS('A')
+'''
+'''
+g.addVertice("A")
+g.addVertice("E")
+g.print()
 
 g.add_aresta("A", "F")
 
@@ -97,4 +131,4 @@ print(ar)
 print("\n")
 g.printGrafo()
 print(g.get_grau_vertice("A"))
-
+'''
--- Digrafo/aresta.py
+++ Digrafo/aresta.py
@@ -4,9 +4,33 @@ class Arestas(object):
         self.arestas = {}
         self.rotulo = rotulo
         self.id = id
-        self.info = []
-    def add_vizinho(self, para, info=[1]):
+        
+    def addAresta(self, para, infoA = {'peso': 1}):
         if para not in self.arestas.keys():
-            self.arestas[para] = info
-    def getInfo(self):
-        return [self.id, self.arestas, self.info]
+            self.arestas[para] = infoA
+            
+    def getInfo(self, para):
+        return self.arestas[para]
+    def getQtdeArestas(self):
+        return len(self.arestas)
+    
+    def getArestas(self):
+        return self.arestas
+    def getArestasInfo(self):
+        arInf = []
+        for a in self.arestas:
+            arInf.append([a, self.arestas[a]])
+        return arInf
+    
+    def getId(self):
+        return self.id
+        
+    def setId(self, id):
+        self.id = id
+        
+
+'''
+ar = Arestas("A", 1)
+ar.addInfoV( {'BST': 0} )
+print(ar.getInfoV())
+'''
